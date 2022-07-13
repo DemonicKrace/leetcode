8687c4dcb23f973aa84f07e8fb67ad76daef0562
unknown <a@a.a>
2022-07-13 16:17:09 +0800

    add 122, 189

---
+++ problem/0122.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) 
+            // add earned profit if positive
+            profit += Math.max(0, prices[i] - prices[i - 1]);
+        return profit;
+    }
+}
---
+++ problem/0189.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        // method 1, reverse, time = O(N), space = O(1)
+        // reverse all
+        // ex: =====>==> -> <==<=====
+        reverse(nums, 0, nums.length - 1);
+        // reverse rotate part
+        // ex: <==<===== -> ==><=====
+        reverse(nums, 0, k - 1);
+        // reverse remaining part
+        // ex: ==><===== -> ==>=====>
+        reverse(nums, k, nums.length - 1);
+    }
+    
+    private void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
