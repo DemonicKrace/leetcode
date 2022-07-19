23141b859871e7750dc9c9878f351f45b70471ea
unknown <a@a.a>
2022-07-19 20:57:35 +0800

    update 21, 234

--- problem/0021.java
+++ problem/0021.java
@@ -9,10 +9,31 @@
  * }
  */
 class Solution {
+
+    // method 1, recursive, time = O(N), space = O(N)
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1==null || l2==null) return l2 == null ? l1 : l2;
         ListNode first = (l2.val < l1.val) ? l2 : l1;
         first.next = mergeTwoLists(first.next, first == l1 ? l2 : l1);
         return first;
+    }
+
+    // method 2, recursive, time = O(N), space = O(1)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode();
+        ListNode head = temp, p1 = list1, p2 = list2;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                temp.next = p1;
+                p1 = p1.next;
+            } else {
+                temp.next = p2;
+                p2 = p2.next;
+            }
+            temp = temp.next;
+        }
+        if (p1 != null) temp.next = p1;
+        if (p2 != null) temp.next = p2;
+        return head.next;
     }
 }
--- problem/0234.java
+++ problem/0234.java
@@ -47,3 +47,48 @@ class Solution {
         return prev;
     }
 }
+
+// 2022-07-19
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // single node
+        if (head.next == null) return true;
+        // find first part end
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // store second list start node, wait to reverse
+        ListNode halfStart = slow.next;
+        // split as two list
+        slow.next = null;
+        // reverse second list
+        ListNode rHead = reverseListNode(halfStart);
+        while (head != null && rHead != null) {
+            // check first list and second list value is same
+            if (head.val != rHead.val) return false;
+            head = head.next;
+            rHead = rHead.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseListNode(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode node = reverseListNode(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}
